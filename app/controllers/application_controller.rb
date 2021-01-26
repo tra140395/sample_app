b6c117b14afa910f8a07958cf806a36b894f49b1
tra140395 <sontra140395@gmail.com>
2021-01-26 15:14:07 +0700

    chapter_6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,14 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   private
-  
+
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
     locale = params[:locale].to_s.strip.to_sym
-    I18n.locale = I18n.available_locales.include?(locale) ? locale : I18n.default_locale
+    return I18n.locale = locale if I18n.available_locales.include?(locale)
+
+    I18n.locale = I18n.default_locale
   end
+
   def default_url_options
     {locale: I18n.locale}
   end

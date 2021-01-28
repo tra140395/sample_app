237ee88794de2869d5322b2d14996bb316e039f3
tra140395 <sontra140395@gmail.com>
2021-01-28 08:23:00 +0700

    chapter_7

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  resources :users, only: %i(new create)
   scope "(:locale)", locale: /en|vi/ do
     resources :microposts
     resources :users

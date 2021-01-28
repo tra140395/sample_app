bd2208dfbb49c1baa829b404273f14561710f9e7
tra140395 <sontra140395@gmail.com>
2021-01-28 13:37:57 +0700

    chapter 8

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,15 @@ class User < ApplicationRecord
     email.downcase!
   end
   has_secure_password
+
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create string, cost: cost
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get "static_pages/home"
+  root to: "static_pages#home"
   get "static_pages/help"
   get "/signup", to: "users#new"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
   post "/signup", to: "users#create"
+  delete "/logout", to: "sessions#destroy"
   resources :users, only: %i(new create)
   scope "(:locale)", locale: /en|vi/ do
     resources :microposts
